3cf2d6b3329a7469658c96899e4ac5f9ef2f861b
AnastasiaChibrovkina <anastasia.chibrovkina@gmail.com>
2025-09-16 13:13:56 +0300

    Чибровкина Анастасия Андреевна БИВТ-25-16 white

---
+++ White.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab1
+{
+    public class White
+    {
+        public bool Task1(double d)
+        {
+            bool answer = false;
+
+            // code here
+            if (d > 0)
+            {
+                answer = true;
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task2(int n)
+        {
+            bool answer = false;
+
+            // code here
+            
+            if (n%2==0)
+            {
+                answer= true;
+            }
+            // end
+
+            return answer;
+        }
+        public int Task3(int a, int b)
+        {
+            int answer = 0;
+
+            // code here
+            if (a>b)
+            {
+                answer = a;
+            }
+            else
+            {
+                answer = b;
+            }
+        // end
+
+        return answer;
+        }
+        public double Task4(double d, double f)
+        {
+            double answer = 0;
+
+            // code here
+            if (Math.Abs(d)>Math.Abs(f))
+            {
+                answer = d;
+            }
+            else
+            {
+                answer = f;
+            }
+            // end
+
+            return answer;
+        }
+        public double Task5(double x)
+        {
+            double answer = 0;
+
+            // code here
+            if (Math.Abs(x)>1)
+            {
+                answer = 1;
+            }
+            else
+            {
+                answer = x;
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task6(double x, double y, double r)
+        {
+            bool answer = false;
+
+            // code here
+            answer = (Math.Abs(x * x + y * y - r * r) <= Math.Pow(10, -4));
+            // end
+
+            return answer;
+        }
+
+        public bool Task7(int n)
+        {
+            //bool answer = false;
+
+            // code here
+            int s = n * n;
+            bool answer = false;
+            if (s-n>2*n)
+            {
+                if (n%2==0)
+                {
+                    answer= true;
+                }
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task8(double L, int T, int M)
+        {
+            bool answer = false;
+
+            // code here
+            answer = (L <= 30 && T + M >= 5 && M % 2 == 0);
+            // end
+
+            return answer;
+        }
+    }
+}
